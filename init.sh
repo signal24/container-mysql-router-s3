7a0a11064a24a773463b45f755364c2325496017
Sean Ferguson <sean@sgnl24.com>
2023-11-02 05:54:06 -0400

    add hostname to S3 path

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 if [ -n "$S3_BUCKET" ]; then
     mkdir /tmp/mysqlrouter
     # mount-s3 --prefix "${S3_PREFIX:-/}" "${S3_BUCKET}" /tmp/mysqlrouter
-    s3fs ${S3_BUCKET}:${S3_PREFIX:-/} /tmp/mysqlrouter -o use_cache=/tmp -o allow_other
+    s3fs ${S3_BUCKET}:${S3_PREFIX:-/}$(hostname)/ /tmp/mysqlrouter -o use_cache=/tmp -o allow_other
 fi
 
 if [ -f /tmp/mysqlrouter/mysqlrouter.conf ]; then
